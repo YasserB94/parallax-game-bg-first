22eb0bca6463eea80cd4e41d45acc1acf58af3b0
YasserB94 <yasserbal@icloud.com>
2022-07-26 20:41:36 +0200

    Configured basic gameloop capped to 60fps

---
+++ Models/GameController.js
@@ -0,0 +1,33 @@
+import InputHandler from "./InputHandler.js";
+import Game from "./Game.js";
+export default class GameController {
+    constructor(canvas) {
+        this.InputHandler = new InputHandler();
+        this.fps = 60;
+        this.game = new Game(canvas, this.InputHandler, this.fps);
+        this.startGameLoop();
+    }
+    startGameLoop() {
+        //Wrapped in Arrow Function to resolve scope issues
+        this.timeSinceLastFrame = 0;
+        window.requestAnimationFrame((currentTime) => {
+            this.gameLoop(currentTime);
+        });
+    }
+    gameLoop(currentTime) {
+        const secondsSinceLastRender = (currentTime - this.timeSinceLastFrame) / 1000;
+        if (secondsSinceLastRender < 1 / this.fps) {
+            window.requestAnimationFrame((currentTime) => {
+                this.gameLoop(currentTime);
+            });
+            return;
+        };
+        this.timeSinceLastFrame = currentTime;
+        this.game.update();
+        this.game.draw();
+        //Wrapped in Arrow Function to resolve scope issues
+        window.requestAnimationFrame((currentTime) => {
+            this.gameLoop(currentTime);
+        });
+    }
+}
